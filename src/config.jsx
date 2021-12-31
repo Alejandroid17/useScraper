4f7c6d5baa560fd870f35e95c8b11d9fa454dc33
Alejandro <alejandrofernandezm94@gmail.com>
2021-12-31 18:14:53 +0100

    chore: add basic scraper hook with a basic configuration

---
+++ src/config.jsx
@@ -0,0 +1,115 @@
+const DEFAULT_SELECTORS = {
+  title: { query: "title" },
+  charset: { query: "meta[charset]", attr: "charset" },
+  description: {
+    query: "meta[name=description]",
+    default: "Empty",
+  },
+  keywords: {
+    query: "meta[name=keywords]",
+    default: null,
+  },
+  author: {
+    query: "meta[name=author]",
+    default: null,
+  },
+  image: {
+    query: "meta[name=image]",
+  },
+  tag: {
+    query: "meta[property=tag]",
+    all: true,
+  },
+  favicon: {
+    query: "link[rel=icon]",
+    attr: "href",
+    all: true,
+  },
+}
+
+const OPEN_GRAPH_SELECTORS = {
+  "og:locale": {
+    query: 'meta[property="og:locale"]',
+  },
+  "og:image": {
+    query: 'meta[property="og:image"]',
+  },
+  "og:image:width": {
+    query: 'meta[property="og:image:width"]',
+  },
+  "og:image:height": {
+    query: 'meta[property="og:image:height"]',
+  },
+  "og:title": {
+    query: 'meta[property="og:title"]',
+  },
+  "og:description": {
+    query: 'meta[property="og:description"]',
+  },
+  "og:type": {
+    query: 'meta[property="og:type"]',
+  },
+  "og:video:url": {
+    query: 'meta[property="og:video:url"]',
+  },
+  "og:video:secure_url": {
+    query: 'meta[property="og:video:secure_url"]',
+  },
+  "og:video:type": {
+    query: 'meta[property="og:video:type"]',
+  },
+  "og:video:width": {
+    query: 'meta[property="og:video:width"]',
+  },
+  "og:video:height": {
+    query: 'meta[property="og:video:height"]',
+  },
+  "og:url": {
+    query: 'meta[property="og:url"]',
+  },
+  "og:site_name": {
+    query: 'meta[property="og:site_name"]',
+  },
+  "og:video:tag": {
+    query: 'meta[property="og:video:tag"]',
+    all: true,
+  },
+}
+
+const TWITTER_SELECTORS = {
+  "twitter:creator": {
+    query: 'meta[name="twitter:creator"]',
+  },
+  "twitter:title": {
+    query: 'meta[name="twitter:title"]',
+  },
+  "twitter:description": {
+    query: 'meta[name="twitter:description"]',
+  },
+  "twitter:card": {
+    query: 'meta[name="twitter:card"]',
+  },
+  "twitter:site": {
+    query: 'meta[name="twitter:site"]',
+  },
+  "twitter:image": {
+    query: 'meta[name="twitter:image"]',
+  },
+}
+
+const FACEBOOK_SELECTORS = {
+  "fb:app_id": {
+    query: 'meta[property="fb:app_id"]',
+  },
+}
+
+const DefaultConfig = {
+  selectors: {
+    ...DEFAULT_SELECTORS,
+    ...OPEN_GRAPH_SELECTORS,
+    ...TWITTER_SELECTORS,
+    ...FACEBOOK_SELECTORS,
+  },
+}
+
+export default DefaultConfig
---
+++ src/useScraper.jsx
@@ -0,0 +1,88 @@
+import { useEffect, useState } from "react"
+import DefaultConfig from "./config"
+import PropTypes from "prop-types"
+
+const useScraper = ({ url, proxyURL, config = DefaultConfig }) => {
+  const [isLoading, setIsLoading] = useState(true)
+  const [data, setData] = useState({})
+  const [error, setError] = useState(null)
+
+  useEffect(() => {
+    reset()
+    doFetch()
+  }, [url, proxyURL])
+
+  const reset = () => {
+    setIsLoading(true)
+    setData({})
+    setError(null)
+  }
+
+  const buildURL = () => {
+    return proxyURL + url
+  }
+
+  const doFetch = async () => {
+    setIsLoading(true)
+
+    const url = buildURL()
+
+    const response = await fetch(url)
+
+    if (!response.ok) {
+      setError(response.statusText)
+      setIsLoading(false)
+      return
+    }
+
+    const htmlText = await response.text()
+
+    parseHTML(htmlText)
+    setIsLoading(false)
+  }
+
+  const parseHTML = (htmlText) => {
+    const parser = new DOMParser().parseFromString(htmlText, "text/html")
+    let data = {}
+
+    for (const [key, tag] of Object.entries(config.selectors)) {
+      const element = tag.all
+        ? parser.querySelectorAll(tag.query)
+        : parser.querySelector(tag.query)
+
+      const attr = tag.attr ? tag.attr : "content"
+
+      const value = element
+        ? attr
+          ? tag.all
+            ? Array.from(element).map((node) => node.getAttribute(attr))
+            : element.getAttribute(attr)
+          : element.textContent
+        : tag.default ?? null
+
+      data = {
+        ...data,
+        [key]: value,
+      }
+    }
+
+    setData(data)
+  }
+
+  return {
+    isLoading,
+    data,
+    error,
+  }
+}
+
+export default useScraper
+
+useScraper.propTypes = {
+  /**
+   * URL to scrape
+   */
+  url: PropTypes.string.isRequired,
+  proxyURL: PropTypes.string.isRequired,
+  config: PropTypes.object,
+}
