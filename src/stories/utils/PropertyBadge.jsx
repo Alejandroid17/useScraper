e9aecbdefdfe08d7700a990ca80bd3888e702730
Alejandro <alejandrofernandezm94@gmail.com>
2022-01-01 14:43:14 +0100

    chore: add logic to check if prop is object in PropertyBadge component

--- src/stories/utils/PropertyBadge.jsx
+++ src/stories/utils/PropertyBadge.jsx
@@ -1,8 +1,10 @@
 import React from "react"
 
 const PropertyBadge = ({ label }) => {
+  label = typeof label === "object" ? JSON.stringify(label) : label
+
   return label ? (
-    <code className="bg-slate-100 py-1 px-2 text-sm rounded leading-none flex items-center text-blue-500">
+    <code className="flex items-center px-2 py-1 text-sm leading-none text-blue-500 rounded bg-slate-100">
       {label}
     </code>
   ) : null
