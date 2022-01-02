3c584ddd5f13af03251e8b0f15d9161da98a3c50
Alejandro <alejandrofernandezm94@gmail.com>
2022-01-02 21:53:02 +0100

    chore: change vite config

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,29 @@
-import { defineConfig } from 'vite'
-import reactRefresh from '@vitejs/plugin-react-refresh'
+import { defineConfig } from "vite"
+import reactRefresh from "@vitejs/plugin-react-refresh"
+import * as path from "path"
+import pkg from "./package.json"
+import camelCase from "camelcase"
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [reactRefresh()]
+  plugins: [reactRefresh()],
+  esbuild: {
+    jsxInject: "import * as React from 'react';",
+  },
+  build: {
+    lib: {
+      entry: path.resolve(__dirname, "src/index.js"),
+      name: camelCase(path.basename(pkg.name)),
+      fileName: (format) => `index.${format}.js`,
+    },
+    rollupOptions: {
+      external: ["react", "react-dom"],
+      output: {
+        globals: {
+          react: "React",
+          "react-dom": "ReactDOM",
+        },
+      },
+    },
+  },
 })
