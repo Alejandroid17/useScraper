7ac2e64f1b23ce2325655231a3532009ac3a64fa
Alejandro <alejandrofernandezm94@gmail.com>
2022-01-02 21:53:13 +0100

    chore: export modules

---
+++ src/index.js
@@ -0,0 +1,16 @@
+import useScraper from "./useScraper"
+import DefaultConfig, {
+  DEFAULT_SELECTORS,
+  OPEN_GRAPH_SELECTORS,
+  TWITTER_SELECTORS,
+  FACEBOOK_SELECTORS,
+} from "./config"
+
+export default useScraper
+export {
+  DefaultConfig,
+  DEFAULT_SELECTORS,
+  OPEN_GRAPH_SELECTORS,
+  TWITTER_SELECTORS,
+  FACEBOOK_SELECTORS,
+}
